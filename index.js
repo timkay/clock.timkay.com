f5bd92dea1875305b5fa821e5889bfde06edb3e8
Tim Kay <timkay@not.com>
2024-07-15 12:41:19 -0700

    WIP

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function update() {
 }
 
 function popout() {
-    if (location === parent.location && window.opener === window) {
+    if (location === parent.location && window.opener === null) {
         let options =  `height=${w}, width=${w}, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no`;
         open('https://clock.timkay.com/', 'clock', options);
         // window.close();
